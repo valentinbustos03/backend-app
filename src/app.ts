c8442897c8289cbde3e08c621fb3d77ee5100f07
Maiden <luccamaidana03@gmail.com>
2025-04-20 21:33:50 -0300

    feat: CRUD table, package shared, package requests

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express from 'express';
+import express from 'express'
+import { tableRouter } from './routes/table.route.js'
 
-const app = express();
+const app = express()
+app.use(express.json())
 
-app.use('/', (req, res ) =>{
-  res.send('Te cojo?');
+app.use('/api/tables', tableRouter)
+
+app.use((_, res) => {
+  res.status(404).send({ message: 'Resource not found' })
 })
 
-app.listen(3000, () =>{
-  console.log("Server running")
+app.listen(3000, () => {
+  console.log('Server runnning on http://localhost:3000/')
 })
