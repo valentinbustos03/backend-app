7d4c6ebdaca7b85ac967ea9849f6990249bb9856
Valentin Bustos <valenbustos3060@gmail.com>
2025-05-02 16:53:01 -0300

    update

--- src/entities/table.entity.ts
+++ src/entities/table.entity.ts
@@ -3,15 +3,16 @@ import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity() //Le dice al MIKRO ORM que "MESA" es una tabla en la base de datos.
 export class Table {
-  @PrimaryKey({ nullable: false, unique: true }) //indica que codigo es una llave primaria
+  @PrimaryKey({ nullable: false, unique: true}) //indica que codigo es una llave primaria
   cod!: string;
 
   @Property({ nullable: false }) //Declara las columnas dentro de la base de datos.
   capacity!: number;
 
-  @Property({ nullable: false })
+  @Property()
   description?: string;
 
   @Property({ nullable: false })
   occupied: boolean = false;
+
 }
